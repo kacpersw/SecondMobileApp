27a0173ebf67b6c46628afbd915b22cb81d901e4
Kacper Świsłocki <swislocki.kacper@gmail.com>
2017-10-03 17:25:10 +0200

    Refactoring

--- app/src/main/java/com/example/kacper/secondapp/MainActivity.java
+++ app/src/main/java/com/example/kacper/secondapp/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.kacper.secondapp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
--- app/src/main/java/com/example/kacper/secondapp/NewElementToDoActivity.java
+++ app/src/main/java/com/example/kacper/secondapp/NewElementToDoActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
-//import android.support.design.widget.Snackbar;
+import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.EditText;
 
@@ -26,8 +26,11 @@ public class NewElementToDoActivity extends Activity {
         EditText type = findViewById(R.id.typeOfItem);
         String typeText = type.getText().toString();
         Singleton.addElementToList(new ToDo(typeText, new Date()));
-        Intent intent = new Intent(this, MainActivity.class);
 
+        Intent intent = new Intent(this, MainActivity.class);
+        Snackbar addMessage = Snackbar.make(findViewById(R.id.layout), "You added new item to list.", Snackbar.LENGTH_LONG);
+
+        addMessage.show();
         startActivity(intent);
     }
 
